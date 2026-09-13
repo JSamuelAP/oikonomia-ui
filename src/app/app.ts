0a8137a4a00e595bcac4bcbb5f41cef9ea8ab6fc
JSamuelAP <sp4619168@gmail.com>
2026-09-12 18:09:53 -0600

    chore: configuración inicial
    - Agregar skill angular-developer
    - Configurar prettier, eslint, husky y lint-staged
    - Instalar PrimeNG
    - Configuración local para no versional licencia de PrimeUI
    - Agregar flujos para GitHub Actions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonDirective } from 'primeng/button';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonDirective],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',

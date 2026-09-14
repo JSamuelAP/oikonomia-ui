dc817e4f9fb254f09d83ed7895eb4602c5fb03da
JSamuelAP <sp4619168@gmail.com>
2026-09-13 20:56:01 -0600

    chore: configurar prettier plugin tailwindcss
    - Agregar plugin para que prettier ordene las clases de tailwindcss
    - Arreglar test que esperaba un titulo en el componente App
    - Configurar regla para que Angular no acepte standalone imports sin
      usar en el template
    - Crear script para limpiar imports sin usar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonDirective } from 'primeng/button';
 
 @Component({
-  imports: [RouterOutlet, ButtonDirective],
+  imports: [RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
